fb691c606b679fdba873761c45f536933f73adec
Maksym-Lytvyn <104524765+Maksym-Lytvyn@users.noreply.github.com>
2023-04-05 14:30:43 +0000

    Deploying to gh-pages from @ Maksym-Lytvyn/goit-js-hw-09@8f82ebbfaa55234c9ed3531674c8f4e7a1e156a5 🚀

---
+++ 01-color-switcher.9ac6c37a.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body"),n=null;t.addEventListener("click",(function(){n=setInterval((function(){console.log("process-started!"),t.setAttribute("disabled","true"),o.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)))}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.removeAttribute("disabled"),console.log("process-abrupted!")}))}();
+//# sourceMappingURL=01-color-switcher.9ac6c37a.js.map
