67b088ef9bd5443464bab9e097b9ebbb100eed01
Максим Литвин / Maksym Lytvyn <104524765+Maksym-Lytvyn@users.noreply.github.com>
2023-04-06 21:12:46 +0300

    up

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,31 +1,71 @@
 // імпортуємо бібліотеку
 import Notiflix from 'notiflix';
 // захоплюємо необхідні елементи
-const form = document.querySelector('form');
-const delayInput = document.querySelector('input[name=delay]');
-const stepInput = document.querySelector('input[name=step]');
-const amountInput = document.querySelector('input[name=amount]');
-
+const form = document.querySelector('.form');
 // створюємо прототип функції для промісу
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    Notiflix.Notify.info('Проміс виконано!');
-  } else {
-    Notiflix.Notify.info('Проміс відхилено!');
+    const shouldResolve = Math.random() > 0.3;
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    });
   }
-}
-
-
-
+  
 // створюємо прослуховувач гадсилання форми
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  createPromise(0.2, 4000);
-})
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+  
+    const delay = Number(form.elements.delay.value);
+    const step = Number(form.elements.step.value);
+    const amount = Number(form.elements.amount.value);
+  
+    for (let i = 0; i < amount; i++) {
+      const position = i + 1;
+      const currentDelay = delay + step * i;
+      createPromise(position, currentDelay)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+        console.log(currentDelay);
+    }
+    Notiflix.Notify.info('Цикл промісів завершено!')
+  });
 
 
 
 
 // інше
 Notiflix.Notify.info('Віконце сповіщень готове!');
+
+//ПЕРЕВІРКОВИЙ КОД
+// const isSuccess = true;
+
+// function examplePromise(){
+//   const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (isSuccess) {
+//       resolve(22);
+//     } else {
+//       reject(44);
+//     }
+//   }, 5000);
+// });
+
+// promise
+//   .then(value => {
+//     Notiflix.Notify.info(value);
+//   })
+//   .catch(error => {
+//     Notiflix.Notify.info(error);
+//   })
+//   .finally(() => Notiflix.Notify.info("Виконання промісу завершено"));
+// }
+// examplePromise();
